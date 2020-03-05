05ab3abced8901be2fb82aa405def15f18b359de
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-03-05 21:49:44 +0900

    integrate collection backend in

--- components/Collection.js
+++ components/Collection.js
@@ -6,35 +6,31 @@ import {Container, Header, Left, Body, Right, Title} from 'native-base';
 import {Content} from 'native-base';
 //import LottieView from 'lottie-react-native';
 import {useFocusEffect} from '@react-navigation/native';
+import utils from '../utils.js';
 
 export default function Collection() {
+  const [loading, setLoading] = useState(true);
   const [collection, setCollection] = useState([]);
-  const [selectedWaifu, setSelectedWaifu] = useState(
-    'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262__340.jpg',
+  const [selectedWaifu, setSelectedWaifu] = useState(null);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      utils.getWaifus().then(data => {
+        setLoading(false);
+        setCollection(data);
+      });
+      return () => {
+        setLoading(true);
+      };
+    }, []),
   );
 
-  useEffect(() => {
-    //fetch("api/collection").then(setImages(res))
-    //test-data
-    const waifus = [
-      'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262__340.jpg',
-      'https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519__340.jpg',
-      'https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544__340.jpg',
-      'https://cdn.pixabay.com/photo/2016/09/07/22/38/cat-1652822__340.jpg',
-      'https://cdn.pixabay.com/photo/2016/03/28/12/35/cat-1285634__340.png',
-      'https://cdn.pixabay.com/photo/2019/11/08/11/56/cat-4611189__340.jpg',
-      'https://cdn.pixabay.com/photo/2015/03/27/13/16/cat-694730__340.jpg',
-      'https://cdn.pixabay.com/photo/2018/04/13/21/24/lion-3317670__340.jpg',
-      'https://cdn.pixabay.com/photo/2017/11/22/08/07/cat-2969932__340.jpg',
-      'https://cdn.pixabay.com/photo/2013/05/17/15/54/cat-111793__340.jpg',
-      'https://cdn.pixabay.com/photo/2017/11/13/07/14/cat-eyes-2944820__340.jpg',
-      'https://cdn.pixabay.com/photo/2017/08/23/08/33/cats-eyes-2671903__340.jpg',
-      'https://cdn.pixabay.com/photo/2018/01/28/12/37/cat-3113513__340.jpg',
-      'https://cdn.pixabay.com/photo/2016/11/23/14/57/adorable-1853372__340.jpg',
-      'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662__340.jpg',
-    ];
-    setCollection(waifus);
-  }, []);
+  if (loading)
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Loading...</Text>
+      </View>
+    );
   return (
     <Container>
       <Header>
@@ -46,14 +42,17 @@ export default function Collection() {
       </Header>
       <Content style={styles.body}>
         <View style={styles.showView}>
-          {/* <LottieView source={require('./animation.json')} autoPlay loop /> */}
-          <Image
-            style={styles.waifuImage}
-            source={{
-              uri: selectedWaifu,
-            }}
-            resizeMode="contain"
-          />
+          {selectedWaifu ? (
+            <Image
+              style={styles.waifuImage}
+              source={{
+                uri: selectedWaifu.imageUrl,
+              }}
+              resizeMode="contain"
+            />
+          ) : (
+            <></>
+          )}
         </View>
         <ScrollView contentContainerStyle={styles.gallery} horizontal={true}>
           {collection.length !== 0 ? (
@@ -66,7 +65,7 @@ export default function Collection() {
                   <Image
                     style={styles.collection}
                     source={{
-                      uri: waifu,
+                      uri: waifu.imageUrl,
                     }}
                   />
                 </TouchableOpacity>
--- utils.js
+++ utils.js
@@ -1,26 +1,39 @@
 import axios from 'axios';
 
+/**********************************************
+ * USER
+ **********************************************/
 const getUser = function() {
   return q('query{user{ id email gems }}').then(data => data.user);
 };
+
+/**********************************************
+ * WAIFUS
+ **********************************************/
 const gacha = function() {
   return q(
     'mutation{gacha(input:{}){id name imageUrl url series{id name imageUrl url}}}',
   ).then(data => data.gacha);
 };
+const getWaifus = function() {
+  return q(
+    'query{user{ waifus{id name imageUrl url series{id name imageUrl url}} }}',
+  ).then(data => data.user.waifus);
+};
 
+/**********************************************
+ * EXERCISE
+ **********************************************/
 const getExercises = function() {
   return q(
     'query{exercises{id name description muscles{name} equipments{name} exerciseImages{path}}}',
   ).then(data => data.exercises);
 };
-
 const getWorkouts = function() {
   return q(
     'query{user{id email workouts{reps exercise{name} createdAt}}}',
   ).then(data => data.user.workouts);
 };
-
 const logExercise = function(exercise, reps) {
   return q(
     `mutation{createWorkout(input:{exerciseId:${parseInt(
@@ -29,6 +42,9 @@ const logExercise = function(exercise, reps) {
   ).then(data => data.createWorkout);
 };
 
+/**********************************************
+ * UTILITY
+ **********************************************/
 const q = async function(query, variables = {}) {
   console.log(query);
   const result = await axios.post('http://localhost:3000/graphql', {
@@ -44,4 +60,5 @@ export default {
   getExercises,
   logExercise,
   getWorkouts,
+  getWaifus,
 };
