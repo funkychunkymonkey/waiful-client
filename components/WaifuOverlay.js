b9d9068dbdc8480c6bdc7a33b7f913dee11ef68d
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-03-07 15:44:48 +0900

    complete waifu overlay functionality

--- components/WaifuOverlay.js
+++ components/WaifuOverlay.js
@@ -1,49 +1,117 @@
 import * as React from 'react';
-import {StyleSheet, Text, TouchableOpacity, FlatList, View} from 'react-native';
-import {Container, Header, Left, Body, Right, Title, Button} from 'native-base';
-import {Content, Icon, Switch} from 'native-base';
-import {useFocusEffect} from '@react-navigation/native';
-import {Overlay, Image} from 'react-native-elements';
-import LinearGradient from 'react-native-linear-gradient';
+import {StyleSheet, Text, Animated, View, Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {Container} from 'native-base';
 
-import Loading from './Loading.js';
+export default function(props) {
+  const [dialogueSpringAnim] = React.useState(new Animated.Value(500));
+  const [dialogueFadeAnim] = React.useState(new Animated.Value(0));
+  const [dialogueBobAnim] = React.useState(new Animated.Value(10));
+  const [gemSpinAnim] = React.useState(new Animated.Value(0));
+  const [isVisible, setIsVisible] = React.useState(props.isVisible);
 
-export default function({waifu, dialogue}) {
+  const sizeMod = 0.5; // use this to control size of the dialogue box
+
+  React.useEffect(() => {
+    if (!isVisible) return;
+    Animated.spring(dialogueSpringAnim, {
+      toValue: 0,
+      duration: 1000,
+    }).start();
+    Animated.timing(dialogueFadeAnim, {
+      toValue: 1,
+      duration: 1000,
+    }).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(dialogueBobAnim, {
+          toValue: 15,
+          duration: 500,
+        }),
+        Animated.timing(dialogueBobAnim, {
+          toValue: 10,
+          duration: 500,
+        }),
+      ]),
+    ).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(gemSpinAnim, {
+          toValue: 1,
+          duration: 250,
+        }),
+        Animated.timing(gemSpinAnim, {
+          toValue: 0,
+          duration: 250,
+        }),
+      ]),
+    ).start();
+  }, [isVisible]);
+
   return (
-    <Overlay
-      isVisible={true}
-      onBackdropPress={() => {}}
-      fullScreen={true}
-      overlayStyle={{
-        backgroundColor: '#00000055',
-        flexDirection: 'column',
-        alignItems: 'flex-end',
+    <View
+      style={{
+        display: isVisible ? 'flex' : 'none',
+        ...styles.overlayWrapper,
       }}
-      containerStyle={{
-        backgroundColor: 'transparent',
-      }}>
-      <View style={{flex: 1}}></View>
-      <View style={{height: 350 * 0.5 + 100}}>
-        <Container
-          style={{
-            width: '100%',
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          <Container
+      onStartShouldSetResponder={() => setIsVisible(false)}>
+      <View
+        style={{
+          ...styles.filler,
+          justifyContent: 'center',
+        }}>
+        {props.gems ? (
+          <View
             style={{
-              backgroundColor: 'white',
-              marginRight: 10,
-              height: 350 * 0.5,
-              padding: 20,
-              borderRadius: 10,
+              backgroundColor: '#0005',
+              alignItems: 'center',
+              paddingTop: 25,
+              paddingBottom: 25,
             }}>
-            <Text adjustsFontSizeToFit style={{fontSize: 24}}>
-              asdf back!
+            <Animated.View
+              style={{
+                marginBottom: 10,
+                transform: [{scaleX: gemSpinAnim}],
+              }}>
+              <Icon name="gem" color={'white'} size={100} />
+            </Animated.View>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 20,
+              }}>
+              + {props.gems} Gems
             </Text>
-          </Container>
-          <Image
-            style={styles.image}
+          </View>
+        ) : (
+          <></>
+        )}
+      </View>
+      <View style={{height: 350 * sizeMod}}>
+        <Container style={styles.overlayRow}>
+          <Animated.View
+            style={{
+              ...styles.dialogue,
+              opacity: dialogueFadeAnim,
+              top: dialogueSpringAnim,
+              height: 350 * sizeMod,
+            }}>
+            <View style={styles.dialogueRightArrow} />
+            <Animated.View
+              style={{
+                ...styles.dialogueDownArrow,
+                bottom: dialogueBobAnim,
+              }}
+            />
+            <Text adjustsFontSizeToFit style={{fontSize: 24}}>
+              {props.dialogue}
+            </Text>
+          </Animated.View>
+          <Animated.Image
+            style={{
+              ...styles.image,
+              opacity: dialogueFadeAnim,
+            }}
             source={{
               uri: 'https://cdn.myanimelist.net/images/characters/6/275276.jpg',
             }}
@@ -51,13 +119,86 @@ export default function({waifu, dialogue}) {
           />
         </Container>
       </View>
-    </Overlay>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // containers
+  overlayWrapper: {
+    flexDirection: 'column',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: '#0005',
+    zIndex: 100,
+    paddingBottom: 100,
+  },
+  filler: {
+    // this is just an empty View that fills up the rest of the space on the screen
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  overlayRow: {
+    // the horizontal row that places the dialogue box & waifu image
+    width: '100%',
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  // waifu dialogue
+  dialogue: {
+    backgroundColor: 'white',
+    marginRight: 10,
+    padding: 20,
+    flex: 1,
+    borderRadius: 10,
+    position: 'relative',
+    zIndex: 1,
+  },
   image: {
     width: 225 * 0.5,
     height: 350 * 0.5,
+    position: 'relative',
+    zIndex: 0,
+  },
+  dialogueRightArrow: {
+    position: 'absolute',
+    zIndex: 100,
+    right: -20,
+    bottom: 50,
+
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderTopWidth: 0,
+    borderRightWidth: 0,
+    borderBottomWidth: 30,
+    borderLeftWidth: 30,
+    borderTopColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderLeftColor: 'white',
+  },
+  dialogueDownArrow: {
+    position: 'absolute',
+    right: 20,
+    // arrow
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderTopWidth: 10,
+    borderRightWidth: 10,
+    borderBottomWidth: 0,
+    borderLeftWidth: 10,
+    borderTopColor: '#ddd',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderLeftColor: 'transparent',
   },
 });
