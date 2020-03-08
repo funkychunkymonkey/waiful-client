13d451c88998e23488a92c66486f0e21d5a47f2e
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-03-08 16:15:41 +0900

    clean up workout detail page

--- color.js
+++ color.js
@@ -1,16 +1,16 @@
 const COLORS = {
   //background
-  bgPrimary: '#fed14d',
-  bgSecondary: '#f7f7f7',
-  bgHighlight: '#ffa880',
+  bgPrimary: '#fed14d', // yellow
+  bgSecondary: '#f7f7f7', // light gray
+  bgHighlight: '#ffa880', // pink
   //text
-  textPrimary: '#fed14d',
-  textSecondary: '#222222',
-  textTitle: '#fff',
-  textLink: '#012EB2',
-  textHighlight: '#ffa880',
+  textPrimary: '#fed14d', // yellow
+  textSecondary: '#222222', // dark gray
+  textTitle: '#fff', // white
+  textLink: '#012EB2', // dark gray
+  textHighlight: '#ffa880', // pink
   //Icon
-  favHeart: '#d62d51',
+  favHeart: '#d62d51', // dark pink
 };
 
 export default COLORS;
